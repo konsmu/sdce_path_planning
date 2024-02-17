90a081d518afed75193556f057d3088b740dfb07
Konstantin Müller <muellerkonsti@web.de>
2024-02-17 23:29:52 +0100

    Updated dist calculation in collision_circles_cost_spiral

--- project/starter_files/behavior_planner_FSM.cpp
+++ project/starter_files/behavior_planner_FSM.cpp
@@ -78,7 +78,9 @@ double BehaviorPlannerFSM::get_look_ahead_distance(const State& ego_state) {
   // the distance you will need to come to a stop while traveling at speed V and
   // using a comfortable deceleration.
 
-  auto look_ahead_distance = velocity_mag * velocity_mag / (2.0 * accel_mag);  // <- Fix This
+  //auto look_ahead_distance = velocity_mag * velocity_mag / (2.0 * accel_mag);  // <- Fix This
+  auto look_ahead_distance = velocity_mag * _lookahead_time + 0.5 * accel_mag * (_lookahead_time * _lookahead_time);
+
 
   // LOG(INFO) << "Calculated look_ahead_distance: " << look_ahead_distance;
 
