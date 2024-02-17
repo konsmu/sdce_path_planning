61a62f03213a966c9dcb6d4b91092a33833d67bd
Konstantin Müller <muellerkonsti@web.de>
2024-02-17 23:39:17 +0100

    Fixed missing paranthesis

--- project/starter_files/cost_functions.cpp
+++ project/starter_files/cost_functions.cpp
@@ -67,10 +67,10 @@ double collision_circles_cost_spiral(const std::vector<PathPoint>& spiral,
           auto actor_center_y =
               obst.location.y + CIRCLE_OFFSETS[c2] * std::sin(actor_yaw);
 
-          // TODO-Distance from circles to obstacles/actor: How do you calculate
+          // TODO-Distance from circles to obstacles/actor: How do you calculate 
           // the distance between the center of each circle and the 
           // obstacle/actor
-          double dist = sqrt((circle_center_x-actor_center_x)*(circle_center_x-actor_center_x + (circle_center_y-actor_center_y)*(circle_center_y-actor_center_y)));  // <- Update
+          double dist = std::sqrt(((circle_center_x-actor_center_x)*(circle_center_x-actor_center_x)) + ((circle_center_y-actor_center_y)*(circle_center_y-actor_center_y)));  // <- Update
 
           collision = (dist < (CIRCLE_RADII[c] + CIRCLE_RADII[c2]));
         }
